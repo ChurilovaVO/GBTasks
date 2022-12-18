922a55e1fb591d70bb36a4659d8d19ee489ce815
Valentina <vansimova@yandex.ru>
2022-12-19 01:48:38 +0300

    Add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+WriteLine("Является ли число a квадратом числа b?");
+Write("Введите число a: ");
+int a= int.Parse(ReadLine());
+Write("Введите число b: ");
+int b= int.Parse(ReadLine());
+
+if (a==b*b)
+{
+    WriteLine("да");
+}
+else
+{
+    WriteLine("нет");
+}
