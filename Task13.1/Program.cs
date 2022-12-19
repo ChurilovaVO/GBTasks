38792e80a0d182b3e04c03bfddbdfdefba784b6c
Valentina <vansimova@yandex.ru>
2022-12-19 22:10:29 +0300

    Task13.1 solved

---
+++ Task13.1/Program.cs
@@ -0,0 +1,21 @@
+﻿//Программа, которая выводит знак сравнения двух чисел
+using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+if (a>b)
+{
+    WriteLine(">");
+}
+else if (a<b)
+{
+    WriteLine("<");
+}
+else
+{
+    WriteLine("=");
+}
+
