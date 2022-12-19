6b8f8b2486fe6b65aa21177208fb7d52b13b045e
Valentina <vansimova@yandex.ru>
2022-12-19 22:11:48 +0300

    Task17 solved

---
+++ Task17/Program.cs
@@ -0,0 +1,21 @@
+﻿//программа, где одно число является квадратом другого
+using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = int.Parse(ReadLine()!);
+WriteLine("Введите второе число: ");
+int b = int.Parse(ReadLine()!);
+
+if (a == b*b)
+{
+   WriteLine($"Число {a} явялется квардратом числа {b}"); 
+}
+else if (b == a*a)
+{
+   WriteLine($"Число {b} явялется квардратом числа {a}"); 
+}
+else
+{
+    WriteLine("Ни одно число не явялется квдратом другого");
+}
+
