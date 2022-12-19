f4b9e3ab5e82976163795026e43f3c6a7bed8a87
Valentina <vansimova@yandex.ru>
2022-12-19 17:47:23 +0300

    Task2 solved

--- HomeWork1/Task2/Program.cs
+++ HomeWork1/Task2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(ReadLine());
+WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(ReadLine());
+int max = a;
+
+if (a<b)
+{
+    WriteLine($"max={b}, min={a} ");
+}
+else WriteLine($"max={a}, min={b} ");
