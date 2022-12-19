6e9e56e66bf4bbf20d20bcdc307342ae5da0bbf8
Valentina <vansimova@yandex.ru>
2022-12-19 22:11:09 +0300

    Task15 solved

---
+++ Task15/Program.cs
@@ -0,0 +1,16 @@
+﻿//Программа, где введенное число делится на 7 и 23 без остатка
+using static System.Console;
+
+WriteLine("Введите число: ");
+int a = int.Parse(ReadLine()!);
+
+
+if (a%7 == 0 && a%23 == 0)
+{
+    WriteLine("Да");
+}
+else
+{
+    WriteLine("Нет");
+}
+
