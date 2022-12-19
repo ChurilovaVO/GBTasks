c683321087f5745e84e4f5d758f53637c8f32018
Valentina <vansimova@yandex.ru>
2022-12-19 19:19:06 +0300

    Task6 solved

---
+++ HomeWork1/Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+WriteLine("Является ли число a четным?");
+Write("Введите число a: ");
+double a= Convert.ToDouble(ReadLine());
+
+if (a%2==0)
+{
+    WriteLine("да");
+}
+else
+{
+    WriteLine("нет");
+}
