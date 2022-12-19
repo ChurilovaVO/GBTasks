08bda363863155eaa1cb835f7af1c6d4789bd2f0
Valentina <vansimova@yandex.ru>
2022-12-19 19:57:42 +0300

    Task8 solved

---
+++ HomeWork1/Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+WriteLine("Введите число: ");
+int a = int.Parse(ReadLine()!);
+int count = 1;
+
+while (count <= a)
+{
+   if (count % 2 == 0) 
+    {
+       Write(count);
+    }
+count = count + 1;
+}
