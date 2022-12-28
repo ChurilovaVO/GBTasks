93fd4b3666493ece2b43974f38140652a0ff8779
Valentina <vansimova@yandex.ru>
2022-12-28 20:44:45 +0300

    Task 21 solved

---
+++ Seminar_3/Task21/Program.cs
@@ -0,0 +1,15 @@
+﻿//bool isParse=int.TryParse(ReadLine(),out int 1))
+//if (!isParse)
+//вывести квадраты чисел от 1 до N
+
+using static System.Console;
+
+WriteLine("Введите число: ");
+int n = Convert.ToInt32(ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    WriteLine(count * count);
+    count++;
+}
