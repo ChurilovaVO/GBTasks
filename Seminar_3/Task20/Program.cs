9518725789ba25f592921e9f036cd2c56aed31e8
Valentina <vansimova@yandex.ru>
2022-12-29 10:20:06 +0300

    Int x and y deleted

--- Seminar_3/Task20/Program.cs
+++ Seminar_3/Task20/Program.cs
@@ -3,9 +3,6 @@
 WriteLine("Введите количество изготовленных деталей: ");
 int number = Convert.ToInt32(ReadLine());
 
-int x = 0;
-int y = 0;
-
 // x = 2(y +y)
 
 WriteLine(number % 6 == 0 && number > 0? $"Токарь сделал {number / 3 * 2}, Полутокари сделали по {number / 6}" : "Токарь ошибся");
