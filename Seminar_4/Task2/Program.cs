ea951ae8f494a72a1cb972184b82a12a199faf21
Valentina <vansimova@yandex.ru>
2023-01-16 18:07:44 +0300

    Task2 solved

---
+++ Seminar_4/Task2/Program.cs
@@ -0,0 +1,19 @@
+﻿using static System.Console;
+WriteLine("Введите число: ");
+int num = int.Parse(ReadLine()!);
+int result = ColNum(num);
+
+WriteLine($"Количество цифр в числе {num} равно {result}");
+
+
+int ColNum(int A)
+{
+    int Col =0;
+
+    while (A>1)
+    {
+        Col=Col+1;
+        A=A/10;
+    }
+    return Col;
+}
