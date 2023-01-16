ad6b9c12e8a660a701f9273473b43bec74b9c81a
Valentina <vansimova@yandex.ru>
2023-01-16 18:13:59 +0300

    Task1 solved

---
+++ Seminar_4/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿//метод, гда вводится число А и выводится произведение от 1 до А
+
+using static System.Console;
+Write("Input number: ");
+int number = int.Parse(ReadLine()!);
+int comp=GetComp(number);
+WriteLine($"Comp number 1...A = {comp}");
+
+
+int GetComp(int A)
+{
+    int resultComp=1;
+    while(A>0)
+    {
+        resultComp=resultComp*A;
+        A--;  
+    }
+    return resultComp;
+}
