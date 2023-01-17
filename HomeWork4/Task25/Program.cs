33d25cf6355b903d9306df07035662c5330561ab
Valentina <vansimova@yandex.ru>
2023-01-17 12:05:44 +0300

    Task25 solved

---
+++ HomeWork4/Task25/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 25: Напишите программу, реализующую метод,
+// который принимает на вход два числа (A и B) и 
+// возводит число A в натуральную степень B с использованием цикла.
+
+// В задаче не использвать стандартный метод Pow!
+
+using static System.Console;
+WriteLine("Введите число A: ");
+int A=int.Parse(ReadLine()!);
+WriteLine("Введите число B: ");
+int B=int.Parse(ReadLine()!);
+
+int DegAB=DegreeAB(A,B);
+WriteLine(DegAB);
+
+
+int DegreeAB(int a, int b)
+{
+    int result=a;
+    int count=0;
+    
+    if (b<1)
+    {
+        WriteLine("Ошибка: введено ненатуральное число В");
+    }
+    while(count<b-1)
+    {
+        result*=a;
+        count++;
+    }  
+    return result;
+}
