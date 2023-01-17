8113e45875018e2e1e0d9e70dd4ac7ec6299ef1c
Valentina <vansimova@yandex.ru>
2023-01-17 14:37:44 +0300

    Task29 solved

---
+++ HomeWork4/Task29/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 29: Напишите программу, реализующую метод, 
+// который формирует массив случайных целых чисел 
+// из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19,6, 1, 33 -> [1, 2, 5, 7, 19,6, 1, 33 ]
+
+using static System.Console;
+int[] Array = GetArray(8);
+PrintArray(Array);
+
+
+
+int[] GetArray(int size)
+{
+    int[] Arr=new int[size];
+    for (int i=0; i < Arr.Length; i++)
+    {
+        Arr[i]=new Random().Next(0,100);
+    }
+    return Arr;
+}
+
+void PrintArray(int[] a)
+{
+   Write("[");
+    for (int i=0; i < a.Length-1; i++)
+    {
+        Write($"{a[i]}, ");
+    }
+    WriteLine($"{a[a.Length-1]}]");
+}
