d607ae86710efbc9127cafb4ad1af3e6d665266f
Valentina <vansimova@yandex.ru>
2023-01-25 01:19:46 +0300

    Saving previous version

--- HomeWork5/Task34/Program.cs
+++ HomeWork5/Task34/Program.cs
@@ -1,81 +1,55 @@
-﻿// Задача 34: Напишите программу, реализующую методы 
-// формирования массива, заполненного случайными 
-// положительными трёхзначными числами, 
+﻿// Задача 34: Напишите программу, реализующую методы
+// формирования массива, заполненного случайными
+// положительными трёхзначными числами,
 // и подсчета количества чётных чисел в массиве.
 
 // [345, 897, 568, 234] -> 2
 
 using static System.Console;
 WriteLine("Введите размер массива: ");
-int size = int.Parse(ReadLine()!);
-Check(size);
+int size = Convert.ToInt32(ReadLine());
 
+if (size>0)
+{
 int[] Array=GetArray(size);
 int ColEvenNum=GetCol(Array);
 PrintArray(Array);
 WriteLine(ColEvenNum);
-
-
-void Check(int S)
-{
-    if(S<1)
-    {
-        WriteLine("Ошибка");
-    }
 }
-
+else
+{
+WriteLine("Ошибка ввода данных");
+}
 
 int[] GetArray(int a)
 {
-    int[] NewArray = new int[a];
-    for (int i = 0; i < NewArray.Length; i++)
-    {
-        NewArray[i]=new Random().Next(100,1000);
-    }
-    return NewArray;
+int[] NewArray = new int[a];
+for (int i = 0; i < NewArray.Length; i++)
+{
+NewArray[i]=new Random().Next(100,1000);
 }
-
+return NewArray;
+}
 
 int GetCol (int[] Arr)
 {
-    int count =0;
+int count =0;
 for (int i = 0; i < Arr.Length; i++)
 {
-    if (Arr[i]%2==0)
-    {
-        count++;
-    }
+if (Arr[i]%2==0)
+{
+count++;
 }
-    return count;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+return count;
+}
 
 void PrintArray(int[] A)
 {
-    Write("[");
-    for (int i = 0; i < A.Length-1; i++)
-    {
-        Write($"{A[i]}, ");
-    } 
-     WriteLine($"{A[A.Length-1]}]");
+Write("[");
+for (int i = 0; i < A.Length-1; i++)
+{
+Write($"{A[i]}, ");
 }
-  
-   
+WriteLine($"{A[A.Length-1]}]");
+}
