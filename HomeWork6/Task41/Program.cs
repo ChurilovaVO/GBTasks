88d01af09c94d707bb9bec94496573f0564d6d7d
Valentina <vansimova@yandex.ru>
2023-01-25 01:08:26 +0300

    Task41 solved

---
+++ HomeWork6/Task41/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+using static System.Console;
+WriteLine("Введите числа через пробел: ");
+string str=ReadLine()!;
+int[] resultArray=GetArryyFromString(str);
+WriteLine($"Количество чисел больше 0 равно {CheckNumbers(resultArray)}");
+
+int[] GetArryyFromString(string Str)
+{
+    string[] stringArray = Str.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    int[] newArray=new int[stringArray.Length];
+    for (int i = 0; i < stringArray.Length; i++)
+    {
+      newArray[i] = Convert.ToInt32(stringArray[i]);
+    }
+    return newArray;
+}
+
+int CheckNumbers(int[] Ar2)
+{
+    int count=0;
+    for (int i = 0; i < Ar2.Length; i++)
+    {
+        if (Ar2[i]>0) count++;
+    }
+    return count;
+}
