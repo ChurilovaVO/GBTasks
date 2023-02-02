83db03aa9744b2fb905a22d5add8ff071e095f15
Valentina <vansimova@yandex.ru>
2023-02-02 15:26:45 +0300

    Method GetNumber corrected

--- HomeWork9/Task68/Program.cs
+++ HomeWork9/Task68/Program.cs
@@ -16,11 +16,8 @@ WriteLine(result);
 
 int GetNumber(int m, int n)
 {
-    if (m == 0)
-        return n + 1;
+    if (m == 0) return n + 1;
     else
-      if ((m != 0) && (n == 0))
-        return GetNumber(m - 1, 1);
-    else
-        return GetNumber(m - 1, GetNumber(m, n - 1));
+      if ((m != 0) && (n == 0)) return GetNumber(m - 1, 1);
+    else return GetNumber(m - 1, GetNumber(m, n - 1));
 }
